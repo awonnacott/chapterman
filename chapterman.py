5c09521fe75b67fd8b8ac1e7838bf4193a4afe31
Andrew <quadcorder@gmail.com>
2014-06-23 18:09:33 -0400

    Added modes
    Added mode objects, now use lists and iteration to create menus
    Added icons where icons should be
    Made only one copy of default DB

--- chapterman.py
+++ chapterman.py
@@ -13,17 +13,68 @@ import db_pickle as db
 #import db_text as db
 #import db_sql as db
 
+class Mode:
+    def __init__(self, cid, name, title, col, cols, new_vals, get_vals):
+        self.cid      = cid
+        self.name     = name
+        self.title    = title
+        self.col      = col
+        self.cols     = cols
+        self.new_vals = new_vals
+        self.get_vals = get_vals
+
 class Interface(wx.Frame):
     def __init__(self):
         self.filename = "default.p"
         self.dirname = os.path.dirname(os.path.realpath(__file__))
         self.db = db.load(self.get_filename())
+        self.mdb = []
+
+        # Create mode object for member type
+        m_new_vals = {}
+        m_get_vals = {}
+        m_new_vals["Name"]       = lambda member : member
+        m_get_vals["Name"]       = lambda member : member
+        m_new_vals["Events"]     = lambda member : "0"
+        m_get_vals["Events"]     = lambda member : str(len(self.get_events(member)))
+        m_new_vals["Attendance"] = lambda member : "0"
+        m_get_vals["Attendance"] = lambda member : self.get_attendance(member)
+        member = Mode('m', "Members", "Membership List", False, ["Name", "Events", "Attendance"], m_new_vals, m_get_vals)
+        self.mdb.append(member)
+
+        # Create mode object for event type
+        e_new_vals = {}
+        e_get_vals = {}
+        e_new_vals["Name"]       = lambda event : event
+        e_get_vals["Name"]       = lambda event : event
+        e_new_vals["Members"]    = lambda event : "0"
+        e_get_vals["Members"]    = lambda event : str(len(self.get_members(event)))
+        event = Mode('e', "Events", "Event List", True, ["Name", "Members"], e_new_vals, e_get_vals)
+        self.mdb.append(event)
+
+        # Create mode object for meeting type
+        a_new_vals = {}
+        a_get_vals = {}
+        a_new_vals["Date"]       = lambda date : date
+        a_get_vals["Date"]       = lambda date : date
+        a_new_vals["Attendance"] = lambda date : "0"
+        a_get_vals["Attendance"] = lambda date : self.get_date_attendance(date)
+        meeting = Mode('a', "Meetings", "Meeting List", True, ["Date", "Attendance"], a_new_vals, a_get_vals)
+        self.mdb.append(meeting)
+
+        # Create mode object for form type
+        f_new_vals = {}
+        f_get_vals = {}
+        f_new_vals["Form"]       = lambda form : form
+        f_get_vals["Form"]       = lambda form : form
+        form = Mode('f', "Forms", "Form List", True, ["Form"], f_new_vals, f_get_vals)
+        self.mdb.append(form)
 
         self.mode = 'e'
         self.app = wx.App(False)
         wx.Frame.__init__(self, None, title="ChapterMan", size=(640, 480), name="ChapterMan")
 
-        self.icon = wx.Icon("icon.png", wx.BITMAP_TYPE_ANY)
+        self.icon = wx.Icon("logo.png", wx.BITMAP_TYPE_ANY)
         self.SetIcon(self.icon)
 
         self.status_bar = wx.StatusBar(self)
@@ -44,22 +95,17 @@ class Interface(wx.Frame):
         self.file_menu.Bind(wx.EVT_MENU, self.on_exit,        self.file_exit)
 
         self.edit_menu = wx.Menu()
-        self.edit_m    = self.edit_menu.Append(-1, "Members",  "Edit Memberhip List")
-        self.edit_e    = self.edit_menu.Append(-1, "Events",   "Edit List of Events")
-        self.edit_a    = self.edit_menu.Append(-1, "Meetings", "Edit Meeting Days")
-        self.edit_f    = self.edit_menu.Append(-1, "Forms",    "Edit List of Forms")
-        self.edit_menu.Bind(wx.EVT_MENU, self.on_edit('m'), self.edit_m)
-        self.edit_menu.Bind(wx.EVT_MENU, self.on_edit('e'), self.edit_e)
-        self.edit_menu.Bind(wx.EVT_MENU, self.on_edit('a'), self.edit_a)
-        self.edit_menu.Bind(wx.EVT_MENU, self.on_edit('f'), self.edit_f)
+        self.edit = {}
+        for mode in self.mdb:
+            self.edit[mode.cid] = self.edit_menu.Append(-1, mode.name, "Edit " + mode.title)
+            self.edit_menu.Bind(wx.EVT_MENU, self.on_edit(mode), self.edit[mode.cid])
 
         self.view_menu = wx.Menu()
-        self.view_e    = self.view_menu.Append(-1, "Events",     "Event View")
-        self.view_a    = self.view_menu.Append(-1, "Attendance", "Meeting View")
-        self.view_f    = self.view_menu.Append(-1, "Forms",      "Form View")
-        self.view_menu.Bind(wx.EVT_MENU, self.on_view('e'), self.view_e)
-        self.view_menu.Bind(wx.EVT_MENU, self.on_view('a'), self.view_a)
-        self.view_menu.Bind(wx.EVT_MENU, self.on_view('f'), self.view_f)
+        self.view = {}
+        for mode in self.mdb:
+            if mode.col:
+                self.view[mode.cid] = self.view_menu.Append(-1, mode.name, mode.title)
+                self.view_menu.Bind(wx.EVT_MENU, self.on_view(mode), self.view[mode.cid])
 
         self.menu_bar = wx.MenuBar()
         self.menu_bar.Append(self.file_menu, "&File")
@@ -67,11 +113,11 @@ class Interface(wx.Frame):
         self.menu_bar.Append(self.view_menu, "&View")
         self.SetMenuBar(self.menu_bar)
 
-    #    self.tool_bar = self.CreateToolBar()
-    #    self.tool_view = self.tool_bar.AddLabelTool(-1, "View", wx.Bitmap("view.png"))
-    #    self.Bind(wx.EVT_TOOL, self.on_tool_view)
-    #    self.tool_bar.Realize()
-    #    self.SetToolBar(self.tool_bar)
+        self.tool_bar = self.CreateToolBar()
+        self.tool_view = self.tool_bar.AddLabelTool(-1, "View", wx.Bitmap("view.png"))
+        self.Bind(wx.EVT_TOOL, self.on_tool_view)
+        self.tool_bar.Realize()
+        self.SetToolBar(self.tool_bar)
 
         self.grid = grid.Grid(self)
 
@@ -91,7 +137,7 @@ class Interface(wx.Frame):
         cols = len(self.db[self.mode])
         for i in range(rows):
             for j in range(cols):
-                self.db['m'+self.mode][i][j] = self.grid.GetCellValue(i, j)
+                self.db['m'+self.mode][i][j] = self.grid.GetCellValue(i, j).rstrip()
 
     def update(self):
         rows = len(self.db['m'])
@@ -110,23 +156,18 @@ class Interface(wx.Frame):
 
     def set_mode(self, mode):
         self.fromgrid()
-        if mode == 'e':
-            self.mode = 'e'
-        elif mode == 'a':
-            self.mode = 'a'
-        elif mode == 'f':
-            self.mode = 'f'
+        self.mode = mode.cid
         self.update()
 
     def get_events(self, member):
         self.fromgrid()
         member_id = self.db['m'].index(member)
-        return [event for event in self.db['me'][member_id] if event.rstrip() != ""]
+        return [event for event in self.db['me'][member_id] if event != ""]
 
     def get_attendance(self, member):
         self.fromgrid()
         member_id = self.db['m'].index(member)
-        present = len([day for day in self.db['ma'][member_id] if day.rstrip() != ""])
+        present = len([day for day in self.db['ma'][member_id] if day != ""])
         total = len(self.db['a'])
         if total == 0: return 0
         return str(10000*present/total/100.0)+"%" # Weird divisions to round result
@@ -134,7 +175,7 @@ class Interface(wx.Frame):
     def get_date_attendance(self, date):
         self.fromgrid()
         date_id = self.db['a'].index(date)
-        present = len([member for member in self.db['ma'] if member[date_id].rstrip() != ""])
+        present = len([member for member in self.db['ma'] if member[date_id] != ""])
         total = len(self.db['a'])
         if total == 0: return 0
         return str(10000*present/total/100.0)+"%" # Weird divisions to round result
@@ -145,7 +186,7 @@ class Interface(wx.Frame):
         member_id = 0
         members = []
         for member in self.db['m']:
-            if self.db['me'][member_id][event_id].rstrip() != "":
+            if self.db['me'][member_id][event_id] != "":
                 members.append(member)
             member_id += 1
         return members
@@ -247,49 +288,19 @@ class Interface(wx.Frame):
         return result
 
     def on_edit(self, mode):
-        new_vals = {}
-        get_vals = {}
-        if mode   == 'm':
-            ititle = "Member List"
-            cols   =["Name", "Events", "Attendance"]
-            new_vals["Name"]       = lambda member : member
-            get_vals["Name"]       = lambda member : member
-            new_vals["Events"]     = lambda member : "0"
-            get_vals["Events"]     = lambda member : str(len(self.get_events(member)))
-            new_vals["Attendance"] = lambda member : "0"
-            get_vals["Attendance"] = lambda member : self.get_attendance(member)
-        elif mode == 'e':
-            ititle = "Event List"
-            cols   =["Name", "Members"]
-            new_vals["Name"]       = lambda event : event
-            get_vals["Name"]       = lambda event : event
-            new_vals["Members"]    = lambda event : "0"
-            get_vals["Members"]    = lambda event : str(len(self.get_members(event)))
-        elif mode == 'a':
-            ititle = "Meeting List"
-            cols   =["Date", "Attendance"]
-            new_vals["Date"]       = lambda date : date
-            get_vals["Date"]       = lambda date : date
-            new_vals["Attendance"] = lambda date : "0"
-            get_vals["Attendance"] = lambda date : self.get_date_attendance(date)
-        elif mode == 'f':
-            ititle = "Form List"
-            cols   =["Form"]
-            new_vals["Form"]       = lambda form : form
-            get_vals["Form"]       = lambda form : form
-
         def edit(event):
-            edit_frame = wx.Frame(self, -1, title=ititle, size=(640, 480), name=ititle)
+            edit_frame = wx.Frame(self, -1, title=mode.title, size=(640, 480))
+            edit_frame.SetIcon(wx.Icon("edit.png", wx.BITMAP_TYPE_ANY))
 
             edit_sizer   = wx.BoxSizer(wx.VERTICAL)
             edit_sizers  = {}
             edit_buttons = {}
             edit_texts   = {}
-            for col in cols:
+            for col in mode.cols:
                 edit_texts[col] = {}
             
             edit_top = wx.BoxSizer(wx.HORIZONTAL)
-            for col in cols:
+            for col in mode.cols:
                 edit_top.Add(wx.StaticText(edit_frame, -1, col), 5)
             edit_sizer.Add(edit_top)
 
@@ -297,10 +308,10 @@ class Interface(wx.Frame):
                 def on_button(event):
                     edit_buttons[item].Unbind(wx.EVT_BUTTON)
                     edit_buttons[item].Destroy()
-                    for col in cols:
+                    for col in mode.cols:
                         edit_texts[col][item].Destroy()
                     edit_sizer.Remove(edit_sizers[item])
-                    self.delete_item(item, mode)
+                    self.delete_item(item, mode.cid)
                     edit_sizer.Layout()
                     edit_frame.Update()
                 return on_button
@@ -309,10 +320,10 @@ class Interface(wx.Frame):
                 def on_button(event):
                     new_item = textctrl.GetValue()
                     textctrl.Clear()
-                    self.add_item(new_item, mode)
+                    self.add_item(new_item, mode.cid)
                     edit_sizers[new_item] = wx.BoxSizer(wx.HORIZONTAL)
-                    for col in cols:
-                        edit_texts[col][new_item] = wx.StaticText(edit_frame, -1, new_vals[col](new_item))
+                    for col in mode.cols:
+                        edit_texts[col][new_item] = wx.StaticText(edit_frame, -1, mode.new_vals[col](new_item))
                         edit_sizers[new_item].Add(edit_texts[col][new_item], 5, wx.ALIGN_CENTER)
                     edit_buttons[new_item] = wx.Button(edit_frame, -1, "-")
                     edit_sizers[new_item].Add(edit_buttons[new_item], 5, wx.ALIGN_CENTER)
@@ -323,10 +334,10 @@ class Interface(wx.Frame):
                 return on_button
 
             item_id = 0
-            for item in self.db[mode]:
+            for item in self.db[mode.cid]:
                 edit_sizers[item] = wx.BoxSizer(wx.HORIZONTAL)
-                for col in cols:
-                    edit_texts[col][item] = wx.StaticText(edit_frame, -1, get_vals[col](item))
+                for col in mode.cols:
+                    edit_texts[col][item] = wx.StaticText(edit_frame, -1, mode.get_vals[col](item))
                     edit_sizers[item].Add(edit_texts[col][item], 5, wx.ALIGN_CENTER)
                 edit_buttons[item] = wx.Button(edit_frame, item_id, "-")
                 edit_sizers[item].Add(edit_buttons[item], 5, wx.ALIGN_CENTER)
--- db_pickle.py
+++ db_pickle.py
@@ -16,3 +16,10 @@ def load(filename):
 
 def same(filename, db): # VERY BAD
     return False
+    file = open(filename, 'rb')
+    db2 = pickle.load(file)
+    file.close()
+    if len(set(db.keys()) ^ set(db2.keys())) != 0: return False
+    for key,val in db.keys():
+    	if set(db2[key]) != set(val): return False
+    return True
